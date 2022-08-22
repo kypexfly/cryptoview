2b134804a9bc702f078859c892a17ec4a24efab4
kypexfly <rczgraphics@live.com>
2022-08-22 16:51:14 -0500

    add routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CoinList from "./pages/Home";
+
 function App() {
-
-  
-
   return (
     <div className="App">
-      <header>
-        Hola
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<CoinList />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
