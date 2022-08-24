6de86db7930c2ea0abf6d536d5cacef66b8f5dfe
kypexfly <rczgraphics@live.com>
2022-08-24 00:52:09 -0500

    update links

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,29 +1,39 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+
+  const useActiveNav = (loc) => {
+    const location = useLocation();
+    if (location.pathname === loc) {
+      return "active";
+    }
+  };
+
   return (
     <header>
       <div id="navbar">
         <div className="navcont">
           <div id="logo">
-            <a href="/">
-              <h3>
-                <i className="fa-regular fa-gem"></i> cryptoview
-              </h3>
-            </a>
+            <Link to="/">
+                <i className="fa-regular fa-gem"></i> CryptoView
+            </Link>
           </div>
 
-          {/* <input className="searchbar" placeholder="search a coin..." /> */}
-          
+          <input className="searchbar" placeholder="search a coin..." />
+
           <div className="nav">
-            <Link to="/converter">
-              <i class="fa-solid fa-calculator"></i> Converter
+            <Link to="/assets" className={useActiveNav("/assets")}>
+            <i className="fa-solid fa-ranking-star"></i> 
             </Link>
-            <Link to="/news">
-              <i class="fa-solid fa-newspaper"></i> News
+            <Link to="/news" className={useActiveNav("/news")}>
+              <i className="fa-solid fa-newspaper"></i> 
             </Link>
-            <Link to="/about">
-              <i class="fa-solid fa-circle-info"></i> About
+            <Link to="/converter" className={useActiveNav("/converter")}>
+              <i className="fa-solid fa-calculator"></i> 
+            </Link>
+            <Link to="/about" className={useActiveNav("/about")}>
+              <i className="fa-solid fa-circle-info"></i>
             </Link>
           </div>
         </div>
