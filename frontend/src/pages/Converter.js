86f4e168068941807f6fe6d25ed792d8571a0d29
kypexfly <rczgraphics@live.com>
2022-08-24 00:54:06 -0500

    add crypto converter calculator

--- frontend/src/pages/Converter.js
+++ frontend/src/pages/Converter.js
@@ -1,11 +1,84 @@
-const Converter = () => {
-  return (
-    <div id="converter">
-      <h1 className="center">Cryptocurrency Converter Calculator</h1>
+import { useState, useEffect } from "react";
 
-      <form>
-        <select name="" id=""></select>
-      </form>
+const Converter = () => {
+
+  const [amount, setAmount] = useState(1)
+  const [leftCoin, setLeftCoin] = useState("bitcoin")
+  const [rightCoin, setRightCoin] = useState("united-states-dollar")
+  const [leftPrice, setLeftPrice] = useState({})
+  const [rightPrice, setRightPrice] = useState({})
+
+  const fetchAssetRates = async (first, second) => {
+
+    // updates both assets price at the same time to get realtime rate conversion 
+
+    setRightPrice({})
+
+    fetch(`/api/rates/${first}`)
+      .then(res => res.json())
+      .then(json => setLeftPrice(json.data))
+      .catch(err => console.log(err))
+
+    fetch(`/api/rates/${second}`)
+      .then(res => res.json())
+      .then(json => setRightPrice(json.data))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    fetchAssetRates(leftCoin, rightCoin)
+  }, [leftCoin, rightCoin])
+
+  let format_asset = Intl.NumberFormat("en", {
+    maximumSignificantDigits: 7
+  });
+
+  return (
+    <div>
+
+      <h1>Cryptocurrency Converter Calculator</h1>
+      <hr />
+
+
+      <div id="converter">
+
+        <input
+          className="set-amount"
+          type="number"
+          defaultValue={1}
+          placeholder="Ingrese el monto al convertidor"
+          onChange={e => setAmount(e.target.value)}
+        />
+
+        <div>
+          <select name="leftSide"
+            onChange={e => setLeftCoin(e.target.value)}>
+
+            <option value="bitcoin">Bitcoin (BTC)</option>
+            <option value="ethereum">Ethereum (ETH)</option>
+            <option value="united-states-dollar">United States Dollar (USD)</option>
+          </select>
+
+          <select name="rightSide" defaultValue={"united-states-dollar"}
+            onChange={e => setRightCoin(e.target.value)}>
+
+            <option value="bitcoin">Bitcoin (BTC)</option>
+            <option value="ethereum">Ethereum (ETH)</option>
+            <option value="united-states-dollar">United States Dollar (USD)</option>
+          </select>
+
+          <div className="conversion-result">
+            <p style={{ fontSize: "20px" }}>
+              <strong>{format_asset.format(amount)}</strong> {leftPrice.symbol} = <strong>
+                {!rightPrice.rateUsd ? (<i className="fas fa-sync fa-spin"></i>) :
+                  (format_asset.format(amount * leftPrice.rateUsd / rightPrice.rateUsd))}
+              </strong> {rightPrice.symbol}
+            </p>
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 };
