e5ecba303788c122bf3bf497c725c2a446fa2cae
kypexfly <rczgraphics@live.com>
2022-08-24 00:51:53 -0500

    add new pages, paths and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CoinList from "./pages/Home";
+// components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// pages
+import Home from "./pages/Home";
+import Asset from "./pages/Asset";
+import Coins from "./pages/Coins";
+import News from "./pages/News";
 import About from "./pages/About";
 import Converter from "./pages/Converter";
 
 function App() {
+
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -12,12 +20,17 @@ function App() {
         <div className="pages">
           <div className="container">
             <Routes>
-              <Route path="/" element={<CoinList />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/assets" element={<Coins />} />
+              <Route path="/assets/:coinid" element={<Asset />} />
+              <Route path="/news" element={<News />} />
               <Route path="/about" element={<About />} />
               <Route path="/converter" element={<Converter />} />
             </Routes>
           </div>
         </div>
+
+        <Footer />
       </BrowserRouter>
     </div>
   );
