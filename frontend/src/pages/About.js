989c367470b3c5dcb5445a5e40c38e48100c4850
kypexfly <rczgraphics@live.com>
2022-08-24 00:52:48 -0500

    add content for About

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,17 +1,31 @@
 const About = () => {
-    return ( 
+    return (
         <div>
             <h1>About</h1>
+            <hr />
 
-<p>
-    cryptoSpace is a project that uses <a href="https://docs.coincap.io/">CoinCap API</a>.
-</p>
+            <div id="about">
 
-            <ul>
-                <li><a href="https://rchu-dev.netlify.app">Developer</a></li>
-            </ul>
+                <strong>CryptoView</strong> is a project that uses <a href="https://docs.coincap.io/">CoinCap API</a>. It provides some tools that everyone can use for free:
+                <ul>
+                    <li>News feed from <a href="https://cryptopanic.com/">CryptoPanic</a></li>
+                    <li>Cryptocurrency Converter Calculator</li>
+                    <li>List of top cryptocurrencies</li>
+                </ul>
+
+                <br />
+
+                <strong>Others</strong>
+                <ul>
+                    <li>
+                        Go to the developer <a href="https://rchu-dev.netlify.app">Website</a> |
+                        <a href="https://github.com/kypexfly"> GitHub</a> |
+                        <a href="https://www.linkedin.com/in/ricardo-chu-zheng/"> LinkedIn</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default About;
