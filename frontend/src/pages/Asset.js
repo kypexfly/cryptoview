906301c2b2cad804c1897e4d108f564309ebe35c
kypexfly <rczgraphics@live.com>
2022-08-26 19:56:55 -0500

    add breadcrum

--- frontend/src/pages/Asset.js
+++ frontend/src/pages/Asset.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate, Link } from 'react-router-dom'
+
+// components
+import OtherCoins from '../components/OtherCoins'
 import Dashboard from '../components/Dashboard'
 
 const Asset = () => {
 
+    const navigate = useNavigate()
     const { coinid } = useParams()
     const [asset, setAsset] = useState({})
 
@@ -18,12 +22,29 @@ const Asset = () => {
     useEffect(() => {
         fetchAsset()
         coinid && (document.title = (coinid + " - CryptoView") || "CryptoView")
-    }, [])
+    }, [coinid])
+
 
     return (
         <div className="container">
-            {!Object.keys(asset).length ?
-                ( undefined ) : <Dashboard asset={asset} />}
+            <div id="asset">
+                <div className="breadcrum">
+                    <button onClick={() => navigate(-1)}><i className="fa-regular fa-circle-left"></i> Go back</button>
+                    <div className='breadcrum-nav'>
+                        / <Link to="/assets">Assets</Link> / <strong><Link to={`/assets/${asset.id}`}>{asset.name}</Link></strong>
+                    </div>
+                </div>
+
+                {!Object.keys(asset).length ?
+                    (undefined) : <Dashboard asset={asset}/>}
+
+                <OtherCoins/>
+                {/* <div id="othercoins" className="dashboard">
+                    <Link to="/assets/ethereum">Ethereum</Link>
+                    <Link to="/assets/shiba-inu">Shiba Inu</Link>
+                </div> */}
+
+            </div>
         </div>
     );
 }
