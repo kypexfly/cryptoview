68338646d6400c9e052301b77460d110d6114fa7
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2022-09-06 15:29:29 -0500

    fix responsive table

--- frontend/src/pages/Coins.js
+++ frontend/src/pages/Coins.js
@@ -48,103 +48,104 @@ const CoinList = () => {
           <strong>UTC</strong>: {new Date().toUTCString()}
         </p>
         <br />
-
-        <table className="coin-table">
-          <thead>
-            <tr>
-              <td>Rank</td>
-              <td>Name</td>
-              <td>
-                <button
-                  type="button"
-                  className={getClassNamesFor('priceUsd')}
-                  onClick={() => requestSort('priceUsd')}
-                >
-                  Price (USD)
-                </button>
-              </td>
-              <td>Supply</td>
-              <td>Max. Supply</td>
-              <td>
-                <button
-                  type="button"
-                  className={getClassNamesFor('marketCapUsd')}
-                  onClick={() => requestSort('marketCapUsd')}
-                >
-                  Market Cap
-                </button>
-              </td>
-
-              <td>
-                <button
-                  type="button"
-                  className={getClassNamesFor('changePercent24Hr')}
-                  onClick={() => requestSort('changePercent24Hr')}
-                >
-                  Change 24h
-                </button>
-              </td>
-            </tr>
-          </thead>
-
-          <tbody>
-            {!assets.length
-              ? (<tr className="coin-load">
-                <td colSpan="100%" className="center">
-                  <div className="loading">
-                    <span>
-                      <i className="fas fa-sync fa-spin"></i> Loading...
-                    </span>
-                  </div>
+        <div className="table-wrap">
+          <table className="coin-table">
+            <thead>
+              <tr>
+                <td>Rank</td>
+                <td>Name</td>
+                <td>
+                  <button
+                    type="button"
+                    className={getClassNamesFor('priceUsd')}
+                    onClick={() => requestSort('priceUsd')}
+                  >
+                    Price (USD)
+                  </button>
                 </td>
-              </tr>)
-              : (items.map((asset, index) => (
-                <tr key={index}>
-                  <td className="center">{asset.rank}</td>
-                  <td>
-                    <img
-                      className="asset-icon"
-                      loading="lazy"
-                      src={`https://assets.coincap.io/assets/icons/${String(
-                        asset.symbol
-                      ).toLowerCase()}@2x.png`}
-                      alt={asset.symbol}
-                    />
-                    <div>
-                      <Link to={`/assets/${asset.id}`}>
-                        <strong>{asset.name}</strong>
-                        <p>
-                          <small className="text-gray">{asset.symbol}</small>
-                        </p>
-                      </Link>
+                <td>Supply</td>
+                <td>Max. Supply</td>
+                <td>
+                  <button
+                    type="button"
+                    className={getClassNamesFor('marketCapUsd')}
+                    onClick={() => requestSort('marketCapUsd')}
+                  >
+                    Market Cap
+                  </button>
+                </td>
+
+                <td>
+                  <button
+                    type="button"
+                    className={getClassNamesFor('changePercent24Hr')}
+                    onClick={() => requestSort('changePercent24Hr')}
+                  >
+                    Change 24h
+                  </button>
+                </td>
+              </tr>
+            </thead>
+
+            <tbody>
+              {!assets.length
+                ? (<tr className="coin-load">
+                  <td colSpan="100%" className="center">
+                    <div className="loading">
+                      <span>
+                        <i className="fas fa-sync fa-spin"></i> Loading...
+                      </span>
                     </div>
                   </td>
-                  <td>${format_asset.format(asset.priceUsd)}</td>
-                  <td>{Number(asset.supply).toLocaleString()}</td>
-                  <td>
-                    {asset.maxSupply > 0
-                      ? Number(asset.maxSupply).toLocaleString()
-                      : '-'}
-                  </td>
-                  <td className="center">
-                    $
-                    {format_compact.format(asset.marketCapUsd).toLocaleString()}
-                  </td>
-                  <td
-                    className={
-                      asset.changePercent24Hr > 0
-                        ? 'green center'
-                        : 'red center'
-                    }
-                  >
-                    {Number(asset.changePercent24Hr)
-                      .toFixed(2)
-                      .toLocaleString()}
-                    %
-                  </td>
-                </tr>)))}
-          </tbody>
-        </table>
+                </tr>)
+                : (items.map((asset, index) => (
+                  <tr key={index}>
+                    <td className="center">{asset.rank}</td>
+                    <td>
+                      <img
+                        className="asset-icon"
+                        loading="lazy"
+                        src={`https://assets.coincap.io/assets/icons/${String(
+                          asset.symbol
+                        ).toLowerCase()}@2x.png`}
+                        alt={asset.symbol}
+                      />
+                      <div>
+                        <Link to={`/assets/${asset.id}`}>
+                          <strong>{asset.name}</strong>
+                          <p>
+                            <small className="text-gray">{asset.symbol}</small>
+                          </p>
+                        </Link>
+                      </div>
+                    </td>
+                    <td>${format_asset.format(asset.priceUsd)}</td>
+                    <td>{Number(asset.supply).toLocaleString()}</td>
+                    <td>
+                      {asset.maxSupply > 0
+                        ? Number(asset.maxSupply).toLocaleString()
+                        : '-'}
+                    </td>
+                    <td className="center">
+                      $
+                      {format_compact.format(asset.marketCapUsd).toLocaleString()}
+                    </td>
+                    <td
+                      className={
+                        asset.changePercent24Hr > 0
+                          ? 'green center'
+                          : 'red center'
+                      }
+                    >
+                      {Number(asset.changePercent24Hr)
+                        .toFixed(2)
+                        .toLocaleString()}
+                      %
+                    </td>
+                  </tr>)))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
