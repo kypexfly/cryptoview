e1fe38cf45da4cb401a0f1ae70c412b02fb6028a
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2022-09-07 14:41:54 -0500

    add logged page, fix news fetch (page and regions)

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,37 +1,62 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuthContext } from '../hooks/useAuthContext'
+import NewsList from '../components/NewsList'
+import { useNewsFeed } from '../hooks/useNewsFeed'
 
 const Home = () => {
   const { user } = useAuthContext()
+  const { news } = useNewsFeed()
 
   useEffect(() => {
     document.title = 'CryptoView'
   }, [])
 
-  return (
-    <div id="home">
-      <section className="landing">
-        <div className="container">
-          <div className="landbox">
-            <h1>Welcome to CryptoView!</h1>
+  return (<>
+    <div id="home" className="landing">
+      <div className="container">
+        <div className="landbox">
+          <h1 className='center'>Welcome to CryptoView!</h1>
+          <hr />
 
-            {!user
-              ? (
-                <>
+          {!user
+            ? (
+              <>
+                <div className="landing-sign center">
                   <p>Sign up to access some interesting features and more!</p>
-                  <div className="landing-sign">
-                    <Link to="/signup" className="btn-link">Sign Up</Link>
-                    <Link to="/login" className="btn-link">Login</Link>
-                  </div>
-                </>)
-              : (<><p>Logged as: <strong>{user.email}</strong>!</p><p>Enjoy this app!</p></>)
-            }
+                  <Link to="/signup" className="btn-link">Sign Up</Link>
+                  <Link to="/login" className="btn-link">Login</Link>
+                </div>
+              </>)
+            : (<p>Logged as: <strong>{user.email}</strong>!</p>)
+          }
 
+        </div>
+      </div>
+    </div >
+    {user && <div className="container">
+      <div id='home-loggedin'>
+        <div>
+          <div className="something">
+            <h2>Mis Cryptos</h2>
+            <hr />
           </div>
         </div>
-      </section >
-    </div >
+        <div>
+          <h2>Últimas noticias</h2>
+          <hr />
+
+          <div className="feed-list">
+            {!news.results
+              ? (<div className='loading'><span><i className="fas fa-sync fa-spin"></i> Loading...</span></div>)
+              : news.results.map((anew, index) => (
+                <NewsList anew={anew} key={index} />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>}
+  </>
   )
 }
 
