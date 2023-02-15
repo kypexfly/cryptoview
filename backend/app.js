57bd8b08e85105604fa56c689532d14194f25a95
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-02-15 09:45:01 -0500

    Remove heroku process

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,21 @@ require('dotenv').config()
 
 // libraries
 const express = require('express')
-const path = require('path')
 const mongoose = require('mongoose')
 
-// routes
-const assetsRoutes = require('./routers/assets')
-const newsRoutes = require('./routers/news')
-const userRoutes = require('./routers/user')
-
+// --------------------------------------------------------------------
+// ------------------------------ HEROKU ------------------------------
 // Heroku dynamically assigns your app a port, so you can't set the port to a fixed number.
-// Heroku adds the port to the env, so you can pull it from there.
-// Switch your listen to this: .listen(process.env.PORT || 5000)
-// That way it'll still listen to port 5000 when you test locally, but it will also work on Heroku.
-
-const app = express()
+// https://stackoverflow.com/questions/28706180/setting-the-port-for-node-js-server-on-heroku
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+// const path = require('path')
+// app.use(express.static(path.join(__dirname, '../frontend/build')))
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+
+const app = express()
+const PORT = process.env.PORT || 4000
 
 // middleware
 app.use(express.json())
@@ -32,21 +30,28 @@ app.use((req, res, next) => {
 
 mongoose.connect(process.env.MONG_URI)
   .then(() => {
-    app.listen(process.env.PORT || 4000, () => {
-      console.log('💚 Connected to DB and listening...')
+    app.listen(PORT, () => {
+      console.log(`💚 Connected to DB and listening to port ${PORT}...`)
     })
   })
   .catch(err => console.log(err))
 
-// Assets route
+// Routes
+const assetsRoutes = require('./routers/assets')
+const newsRoutes = require('./routers/news')
+const userRoutes = require('./routers/user')
 
 app.use('/api/assets', assetsRoutes)
 app.use('/api/news', newsRoutes)
 app.use('/api/user', userRoutes)
 
+// --------------------------------------------------------------------
+// ------------------------------ HEROKU ------------------------------
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html;
 // (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  // eslint-disable-next-line n/no-path-concat
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-})
+// app.get('*', (req, res) => {
+//   // eslint-disable-next-line n/no-path-concat
+//   res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+// })
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
