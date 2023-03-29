687b508ba19b4aaf2c4dad6a3425dcab9f82fb4d
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-03-29 12:11:03 -0500

    Add tailwindcss and rubik font

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,10 @@ import App from './App'
 import { AuthContextProvider } from './context/AuthContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import './index.css'
+import '@fontsource/rubik'
+import '@fontsource/rubik/700.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 const queryClient = new QueryClient()
