60b42fad0bfc47ac00ad1e80e6848420dcb402b5
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-04-03 16:39:13 -0500

    Disable login and signup

--- client/netlify/functions/controllers/userController.ts
+++ client/netlify/functions/controllers/userController.ts
@@ -1,40 +1,43 @@
-import { User } from '../models/userModel'
-import jwt from 'jsonwebtoken'
-import { connectToDatabase } from '../helpers/connectToDatabase'
+// import { User } from '../models/userModel'
+// import jwt from 'jsonwebtoken'
+// import { connectToDatabase } from '../helpers/connectToDatabase'
+import { Request, Response } from 'express'
 
-const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.JWTSECRET, { expiresIn: '3d' })
-}
+// const createToken = (_id:string) => {
+//   return jwt.sign({ _id }, process.env.JWTSECRET, { expiresIn: '3d' })
+// }
 
 // login a user
-const loginUser = async (req, res) => {
-  const { email, password } = req.body
+const loginUser = async (req:Request, res:Response) => {
+  // const { email, password } = req.body
 
   try {
-    await connectToDatabase()
-    const user = await User.login(email, password)
+    // await connectToDatabase()
+    // const user = await User.login(email, password)
 
-    // create a token
-    const token = createToken(user._id)
+    // // create a token
+    // const token = createToken(user._id)
 
-    res.status(200).json({ email, token })
+    // res.status(200).json({ email, token })
+    throw Error('Login is disabled')
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
 }
 
 // signup a user
-const signupUser = async (req, res) => {
-  const { email, password } = req.body
+const signupUser = async (req:Request, res:Response) => {
+  // const { email, password } = req.body
 
   try {
-    await connectToDatabase()
-    const user = await User.signup(email, password)
+    // await connectToDatabase()
+    // const user = await User.signup(email, password)
 
-    // create a token
-    const token = createToken(user._id)
+    // // create a token
+    // const token = createToken(user._id)
 
-    res.status(200).json({ email, token })
+    // res.status(200).json({ email, token })
+    throw Error('Login is disabled')
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
