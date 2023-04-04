8ad6c15351c975775a898bc33462682323eb59ec
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-04-04 09:34:33 -0500

    change to bcryptjs

--- client/netlify/functions/api.ts
+++ client/netlify/functions/api.ts
@@ -6,7 +6,7 @@ const app = express();
 // Routes
 import { assetsRouter } from './routers/assets'
 import { newsRouter } from './routers/news';
-// import { userRouter } from './routers/user';
+import { userRouter } from './routers/user';
 
 app.use(express.json());
 
@@ -19,7 +19,7 @@ app.use(express.json());
 // Router
 app.use('/.netlify/functions/api/assets', assetsRouter);  
 app.use('/.netlify/functions/api/news', newsRouter); 
-// app.use('/.netlify/functions/api/user', userRouter); 
+app.use('/.netlify/functions/api/user', userRouter); 
 
 const handler = serverless(app)
 
--- client/netlify/functions/controllers/userController.ts
+++ client/netlify/functions/controllers/userController.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { connectToDatabase } from '../helpers/connectToDatabase'
 import { Request, Response } from 'express'
 import { z } from 'zod'
